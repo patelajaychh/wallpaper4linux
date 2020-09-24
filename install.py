6d3050f9bdd6df07467d37ff3c9954559ccea672
Ajay <patelajaychh@gmail.com>
2020-09-25 01:05:40 +0530

    ..

--- install.py
+++ install.py
@@ -1,9 +1,14 @@
-#!/usr/local/bin/python3
+#!/usr/bin/python3
 
 import os
 
 home_dir = os.getenv('HOME')
 project_dir = home_dir+'/wallpaper4linux'
+python_home = None
+while 1:
+    python_home = input('Enter correct python3 location ... ')
+    if os.path.isfile(python_home):
+       break 
 
 cwd = os.getcwd()
 print(project_dir, cwd)
@@ -12,5 +17,5 @@ if project_dir != cwd:
     print('Project is not in home directory. \nPlease move project dir into user HOME directory and follow installation instructions.')
     exit()
 else:
-    os.system(f'echo "/usr/local/bin/python3 {home_dir}/wallpaper4linux/app.py>>/dev/null&" >> ~/.bashrc')
+    os.system(f'echo "{python_home} {home_dir}/wallpaper4linux/app.py>>/dev/null&" >> ~/.bashrc')
     os.system('bash')
